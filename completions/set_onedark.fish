3111e1b1dcc214fb52f50e9cc3a09ce8a0212f29
rkbk60 <b8csmlplg@gmail.com>
2018-06-11 21:18:01 +0900

    add set_onedark completion

---
+++ completions/set_onedark.fish
@@ -0,0 +1,5 @@
+# set_onedark
+complete -c set_onedark -f -d 'apply onedark colorscheme to your terminal'
+
+complete -c set_onedark -s b -l set-background -d 'set background color'
+complete -c set_onedark -s h -l help -d 'show help message'
